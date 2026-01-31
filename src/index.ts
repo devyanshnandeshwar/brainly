711b5dc1e42a501bc98a56decaa51ad9c7d1f482
devyanshnandeshwar <devyansh.cloud@gmail.com>
2026-01-31 14:33:32 +0530

    Initialization

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import express from 'express';
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+});
+
+app.post("/api/v1/signin", (req, res) => {
+
+});
+
+app.get("/api/v1/content", (req, res) => {
+
+});
+
+app.delete("/api/v1/content", (req, res) => {
+
+});
